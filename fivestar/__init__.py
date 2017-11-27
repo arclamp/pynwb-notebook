7cae1276d1db08e2373147f20fd56ee95410a780
Roni Choudhury <roni.choudhury@kitware.com>
2017-11-27 14:38:23 -0500

    Allow constructor to take an initial message

--- fivestar/__init__.py
+++ fivestar/__init__.py
@@ -8,3 +8,9 @@ class Messager(widgets.DOMWidget):
     _view_module_version = Unicode('0.1.0').tag(sync=True)
 
     message = Unicode('hello, world').tag(sync=True)
+
+    def __init__(self, initial_message=None):
+        super(Messager, self).__init__()
+
+        if initial_message is not None:
+            self.message = initial_message
