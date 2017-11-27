4ee2bdd2e37548503404c3dc5ea281de70cc9fff
Roni Choudhury <roni.choudhury@kitware.com>
2017-11-27 14:34:38 -0500

    Add statefulness to example widget

--- fivestar/__init__.py
+++ fivestar/__init__.py
@@ -6,3 +6,5 @@ class Messager(widgets.DOMWidget):
     _view_name = Unicode('Messager').tag(sync=True)
     _view_module = Unicode('messager').tag(sync=True)
     _view_module_version = Unicode('0.1.0').tag(sync=True)
+
+    message = Unicode('hello, world').tag(sync=True)
